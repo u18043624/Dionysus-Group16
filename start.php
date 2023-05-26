93da563f1d3ebc07d1b8740f93a8987891c59a87
Nini <102977834+u21537144@users.noreply.github.com>
2023-05-26 13:48:06 +0200

    Add files via upload

---
+++ start.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Dionysus</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+
+<style>
+  #Background {
+    background-image: url('images/pexels-domeniile-blaga-1522594.jpg');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+  }
+  h1 {
+    font-weight: bold;
+    text-shadow: 2px 2px 4px #000000;
+    /* color:#722F37; */
+  }
+</style>
+<body>
+  <!-- <img src="images/pexels-domeniile-blaga-1522594.jpg" alt="">	 -->
+  <div class="container-fluid">
+    <div class="row" id="Background" style="height: 75vh;  ">
+      <div class="col-12 align-self-center text-center">
+        <h1 class="text-white" style="font-family:Georgia, 'Times New Roman', Times, serif;">WELCOME TO DIONYSUS</h1>
+        <!-- <img src="images/logo.png" alt="Logo" width="200"> -->
+        
+       
+      </div>
+    </div>
+    <div class="row" style="height: 25vh; background-image: linear-gradient(to bottom right, black, #722F37);">
+      <div class="col-12 align-self-center text-center">
+        
+        <a href="login.php" class="btn btn-light btn-lg">START</a>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
